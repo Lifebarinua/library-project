5cfb6b430952478a7e3fa84bc78e765bc2450ba9
Lifebarinua <barinualife@gmail.com>
2025-08-23 20:27:55 +0100

    construct myLibery and function addBookToLibrary

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Book(title, author, pages, read) {
   this.pages = "228";
   this.read = "";
 
-  this.info = function() {
+  this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`;
   };
 }
@@ -20,7 +20,7 @@ console.log(theHobbit.info());
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
-  this.sayName = function() {
+  this.sayName = function () {
     console.log(this.name)
   };
 }
@@ -33,3 +33,28 @@ player2.sayName(); // logs 'also steve'
 
 Object.getPrototypeOf(player1) === Player.prototype; // returns true
 Object.getPrototypeOf(player2) === Player.prototype; // returns true
+
+
+
+
+
+const myLibrary = [];
+
+function Book(tittle, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = "Holy Bible King James version";
+  this.author = "The Holy Spirit";
+  this.pages = "1396";
+  this.read = "read"
+}
+
+
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+};
+
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
